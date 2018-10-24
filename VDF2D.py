7e673591a528776847fc2339068e123cfff88eca
EshitaJ <EJ515@ic.ac.uk>
2018-10-24 15:26:51 +0100

    Updated code for 2D VDF

--- VDF2D.py
+++ VDF2D.py
@@ -28,60 +28,63 @@ class VDF:
             T_perp=constants["T_perp"],
             T_par=constants["T_par"],
             B=constants["B"]):
-        
+
         self.n = n
         self.T_perp = T_perp
         self.T_par = T_par
         self.B = B
-        
+
         self.V_A = (plasmapy.physics.parameters.Alfven_speed(
                 B*u.T,
                 n*u.m**-3,
                 ion="p+"))/(u.m/u.s)
 
-    def BiMax(self, v_perp, v_par, v, n):
-        par_thermal_speed = np.sqrt(2 * cst.k * self.T_par / cst.mp)
-        perp_thermal_speed = np.sqrt(2 * cst.k * self.T_perp / cst.mp)
+    def BiMax(self, v_par, v_perp, v, n):
+        par_thermal_speed = np.sqrt(2 * cst.k * self.T_par / cst.m_p)
+        perp_thermal_speed = np.sqrt(2 * cst.k * self.T_perp / cst.m_p)
 
-        bimax_value = self.n \
-            / (par_thermal_speed * np.square(perp_thermal_speed))\
-            * np.exp(-np.square(v_perp - v) / perp_thermal_speed)\
-            * np.exp(-np.square(v_perp) / par_thermal_speed)
-            
+        bimax_value = n \
+            / ((np.pi**1.5) * par_thermal_speed * np.square(perp_thermal_speed))\
+            * np.exp(-np.square((v_par - v) / par_thermal_speed))\
+            * np.exp(-np.square((v_perp) / perp_thermal_speed))
+
         return bimax_value
-    
+
     def VDF_plot(
             self,
-            core_fraction = 0.8,
-            meshgrid_points=5000,
+            core_fraction=0.8,
+            meshgrid_points=600,
             v_perp_min=-1e6,
             v_perp_max=1e6,
             v_par_min=-1e6,
             v_par_max=1e6):
-        
+
         vperp = np.linspace(v_perp_min, v_perp_max, meshgrid_points)
         vpar = np.linspace(v_par_min, v_par_max, meshgrid_points)
 
         x, y = np.meshgrid(vperp, vpar)
-        
+        print(core_fraction*self.n)
+        print()
+        print((1-core_fraction)*self.n)
+
         core = self.BiMax(x, y, 0, core_fraction*self.n)
         beam = self.BiMax(x, y, self.V_A, (1-core_fraction)*self.n)
-        
+
         z = core + beam
-        
-        
+
+
         C = plt.contour(x, y, z)
         plt.rc('text', usetex=True)
         plt.xlabel("$V_{\parallel}$   (m/s)", fontsize=20)
         plt.ylabel("$V_{\perp}$   (m/s)", fontsize=20)
         plt.clabel(C, inline = 1, fontsize = 10)
-        
+
         plt.show()
-        
+
 
 """parser = argparse.ArgumentParser()
 parser.add_argument('-n', type=int, action=)"""
 
 if __name__ == '__main__':
-    test = VDF
+    test = VDF()
     test.VDF_plot()
