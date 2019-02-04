d91d58a06572dc27e699e209b4f3acbc38c0fb5a
Henrywild <wild.henry@gmail.com>
2019-02-04 21:41:10 +0000

    Changed SW to flow along y axis

--- utils.py
+++ utils.py
@@ -5,10 +5,10 @@ import numpy as np
 def BiMax(x, y, z, v_A, T_x, T_y, T_z, n, core_fraction, is_core, bulk_velocity=700000):
     if is_core:
         n_p = core_fraction * n
-        x = x - bulk_velocity
+        y = y - bulk_velocity
     else:
         n_p = (1 - core_fraction) * n
-        x = x - v_A - bulk_velocity
+        y = y - v_A - bulk_velocity
 
     norm = n_p * (cst.m_p/(2 * np.pi * cst.k))**1.5 / np.sqrt(T_x * T_y * T_z)
     exponent = -((z**2/T_z) + (y**2/T_y) + (x**2/T_x)) \
